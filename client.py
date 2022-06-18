dadaa60b79569c2bd4ec00aec0e1159f9299a153
Vladislav Pochernin <vspochernin@gmail.com>
2022-06-18 10:18:57 +0300

    initial commit (echo example)

---
+++ client.py
@@ -0,0 +1,11 @@
+import socket
+
+# Адрес и порт сервера.
+HOST = "192.168.0.103"
+PORT = "65432"
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b'Hello, world')
+    data = s.recv(1024)
+print('Received', repr(data))
---
+++ server.py
@@ -0,0 +1,17 @@
+import socket
+
+# Адрес и порт сервера.
+HOST = "192.168.0.103"
+PORT = "65432"
+
+# Открытие интернет-сокета IPv4 через протокол TCP.
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT)) # Привязка сокета к адресу.
+    s.listen() # Разрешение серверу принимать соединения.
+    conn, addr = s.accept() # Принимаем соединение из addr с сокетом conn.
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data: break
+            conn.sendall(data)
