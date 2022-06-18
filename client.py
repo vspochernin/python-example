3266635e00dfdfb8d3ccb832095a2ba86f32ba90
Vladislav Pochernin <vspochernin@gmail.com>
2022-06-18 12:32:30 +0300

    change IP to local

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ import random
 from time import sleep
 
 # Адрес и порт сервера.
-HOST = "192.168.0.103"
+HOST = "127.0.0.1"
 PORT = 65432
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
