071b87d6d03efb774de2d870da3a85ff7e5bb651
Vladislav_Pochernin <vspochernin@gmail.com>
2022-06-18 11:26:20 +0300

    change variable names

--- client.py
+++ client.py
@@ -9,8 +9,8 @@ PORT = 65432
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     while True:
-        data = "Сообщение серверу под номером " + str(random.randint(0, 1000))
-        s.sendall(data.encode())
-        message = s.recv(1024).decode()
-        print("Получено сообщение:", message)
+        output_message = "Сообщение серверу под номером " + str(random.randint(0, 1000))
+        s.sendall(output_message.encode())
+        input_message = s.recv(1024).decode()
+        print("Получено сообщение:", input_message)
         sleep(random.randint(1,2))
