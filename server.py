1ceb259e1872cdf02ea7a4367e289d9966966889
Vladislav Pochernin <vspochernin@gmail.com>
2022-06-18 11:24:37 +0300

    update server.py

--- server.py
+++ server.py
@@ -7,12 +7,14 @@ PORT = 65432
 # Открытие интернет-сокета IPv4 через протокол TCP.
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT)) # Привязка сокета к адресу.
-    s.listen() # Разрешение серверу принимать соединения.
+    s.listen() # Разрешаем серверу принимать соединения.
     conn, addr = s.accept() # Принимаем соединение из addr с сокетом conn.
     with conn:
-        print('Connected by', addr)
+        print('Установлено соединение с', addr)
         while True:
-            data = conn.recv(1024) # Читаем отправленные нам данные.
-            if not data: break
-            conn.sendall(data) # Отправляем их назад.
-print("Exit")
+            input_message = conn.recv(1024).decode()
+            print("Получено сообщение:", input_message)
+            parts = input_message.split(' ')
+            message_number = parts[len(parts) - 1]
+            output_message = "Сервером успешно было получено сообщение с номером " + message_number
+            conn.sendall(output_message.encode())
